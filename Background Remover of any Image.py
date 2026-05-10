e5be9faa3642ef3e5615ac3529fa690426c3db84
Kunal Yelgate <kunalyelgatew@gmail.com>
2026-05-10 22:18:41 +0530

    Add background remover functionality for images

---
+++ Background Remover of any Image.py
@@ -0,0 +1,17 @@
+
+from rembg import remove 
+from PIL import Image
+
+input_path = 'photo.jpg'
+
+output_path = 'photo.jpg'
+
+inp = Image.open(input_path)
+
+output = remove(inp)
+
+output.save(output_path)
+
+Image.open(output_path)
+
+
